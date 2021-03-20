9e3c0fea460bf08316e0c0e40f6b64bbd5c6b9bd
Andrey Okonetchnikov <andrey@okonet.ru>
2021-03-20 11:40:18 +0100

    Use [...slug] and TS

--- pages/[collection]/[...slug].js
+++ pages/[collection]/[...slug].js
@@ -14,7 +14,7 @@ export async function getStaticPaths() {
         paths: [{
             params: {
                 collection: "static",
-                slug: "page"
+                slug: ["page"]
             },
         }],
         fallback: true,
